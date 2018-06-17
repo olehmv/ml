6ba6f7ff460c67acdd3b00feb80c8f7c849905f6
oleg_melnychuk@epam.com <oleg_melnychuk@epam.com>
2018-06-17 16:52:01 +0300

    'ex-1'

--- machine-learning-ex1/ex1/gradientDescent.m
+++ machine-learning-ex1/ex1/gradientDescent.m
@@ -16,18 +16,22 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
+    x = X(:,2);
+    h = theta(1) + (theta(2)*x);
 
+    thetaZero = theta(1) - alpha * (1/m) * sum(h-y);
+    thetaOne  = theta(2) - alpha * (1/m) * sum((h - y) .* x);
 
-
-
-
-
-
+    theta = [thetaZero; thetaOne];
+    disp(computeCost(X, y, theta));
     % ============================================================
 
     % Save the cost J in every iteration    
     J_history(iter) = computeCost(X, y, theta);
 
 end
+
+disp('-----------------');
+disp(min(J_history));
 
 end
--- machine-learning-ex1/ex1/plotData.m
+++ machine-learning-ex1/ex1/plotData.m
@@ -22,4 +22,10 @@ figure; % open a new figure window
 
 % ============================================================
 
+plot(x, y, 'rx', 'MarkerSize', 10); % Plot the data 
+title('Population Profitability', "fontsize", 16);
+ylabel('Profit in $10,000s'); % Set the y?axis label 
+xlabel('Population of City in 10,000s'); % Set the x?axis label 
+
+
 end
